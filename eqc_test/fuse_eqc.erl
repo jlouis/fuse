499e96880df12509b8b5062d1aff08cc9498abcb
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-03-11 11:38:52 +0100

    Introduce more QuickCheck model before implementing code.

--- eqc_test/fuse_eqc.erl
+++ eqc_test/fuse_eqc.erl
@@ -9,31 +9,99 @@
 	installed = []
 }).
 
+fuses() -> [fuse_a, fuse_b, fuse_c, fuse_d].
+
 g_name() ->
-	oneof([fuse_a, fuse_b, fuse_c, fuse_d]).
+	oneof(fuses()).
 
 g_options() ->
-	return([{policy, {counter, 5}}]).
+	return([{policy, {counter, 2}}]).
 
+%%% install/2 puts a new fuse into the system
+%%% ---------------------
 install(Name, Opts) ->
 	fuse:install(Name, Opts).
 
 install_args(_S) ->
 	[g_name(), g_options()].
 
-install_next(#state{ installed = Is } = S, _V, [Name, _Opts]) ->
-	Is2 = Is -- [Name],
-	S#state { installed = [Name | Is2] }.
+install_next(#state{ installed = Is } = S, _V, [Name, Opts]) ->
+	[{policy, {counter, Count}}] = Opts,
+	T = {Name, Count},
+	S#state { installed = lists:keystore(Name, 1, Is, T) }.
 
-ask(Name) ->
+install_post(_S, _, R) ->
+	eq(R, ok).
+
+%%% reset/1 resets a fuse back to its policy standard
+reset(Name) ->
+	fuse:reset(Name).
+	
+reset_pre(#state { installed = [] }) -> false;
+reset_pre(#state { installed = [_|_] }) -> true.
+
+reset_args(S) ->
+	[oneof(names(S))].
+
+reset_post(_S, _, Ret) ->
+	eq(Ret, ok).
+
+%%% ask_pos/1 asks about the state of a fuse that exists
+%%% ---------------------
+ask_pos(Name) ->
 	fuse:ask(Name).
 	
-ask_pre(#state { installed = [] }) -> false;
-ask_pre(#state { installed = [_|_]}) -> true.
+ask_pos_pre(#state { installed = [] }) -> false;
+ask_pos_pre(#state { installed = [_|_]}) -> true.
 
-ask_args(#state { installed = Is }) ->
-	[oneof(Is)].
+ask_pos_args(S) ->
+	[oneof(names(S))].
 
+count(Name, #state { installed = Inst }) ->
+	{Name, Count} = lists:keyfind(Name, 1, Inst),
+	Count.
+
+count_state(0) -> blown;
+count_state(_N) -> ok.
+
+ask_pos_post(S, [Name], Ret) ->
+	eq(Ret, count_state(count(Name, S))).
+	
+%%% ask_neg/1 asks about the state of a fuse which does not exist
+ask_neg(Name) ->
+	fuse:ask(Name).
+	
+ask_neg_pre(S) ->
+	case fuses() -- names(S) of
+	    [] -> false;
+	    [_|_] -> true
+	end.
+	
+ask_neg_args(S) ->
+	[oneof(fuses() -- names(S))].
+	
+ask_neg_post(_S, _, Ret) ->
+	eq(Ret, {error, not_found}).
+	
+%%% melt/1 melts the fuse a little bit
+%%% ---------------------
+melt(Name) ->
+	fuse:melt(Name).
+	
+melt_pre(#state { installed = [] }) -> false;
+melt_pre(#state { installed = [_|_]}) -> true.
+
+melt_args(S) ->
+	[oneof(names(S))].
+
+melt_next(#state { installed = Is } = S, _V, [Name]) ->
+	{Name, Count} = lists:keyfind(Name, 1, Is),
+	S#state { installed = lists:keystore(Name, 1, Is, {Name, case Count of 0 -> 0; N -> N-1 end}) }.
+
+melt_post(_S, _, Ret) ->
+	eq(Ret, ok).
+
+%%% Property
 prop_model() ->
 	?FORALL(Cmds, commands(?MODULE, #state{}),
 	  begin
@@ -44,3 +112,9 @@ prop_model() ->
 	  		io:format("History: ~p\nState: ~p\nResult: ~p\n", [H, S, R]),
 	  		aggregate(command_names(Cmds), R == ok))
 	  end).
+
+%%% INTERNALS
+%%% ---------------------
+
+names(#state { installed = Is }) ->
+	[N || {N, _} <- Is].

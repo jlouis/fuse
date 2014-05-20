60138a4218196e2858d39b15eb867974f07c04bd
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-05-21 00:24:27 +0200

    Introduce a stress test.

---
+++ stress/stress.erl
@@ -0,0 +1,43 @@
+-module(stress).
+
+-export([run/0]).
+-compile(inline).
+
+-define(CORES, 8).
+-define(COUNT, 3 * 1000*1000).
+-define(TIMEOUT, 30*1000).
+-define(FUSE, test_fuse).
+
+setup() ->
+	application:load(sasl),
+	application:set_env(sasl, sasl_error_logger, false),
+	application:set_env(sasl, errlog_type, error),
+	error_logger:tty(false),
+	application:start(sasl),
+	application:start(folsom),
+	application:start(fuse),
+	ok.
+
+run() ->
+    setup(),
+    ok = fuse:install(?FUSE, {{standard, 20, 60}, {reset, 30*1000}}),
+    timer:tc(fun() -> run(?CORES) end).
+    
+run(CoreCount) ->
+     Mgr = self(),
+     Pids = [spawn_link(fun() -> loop(Mgr, ?COUNT) end) || _ <- lists:seq(1, CoreCount)],
+     collect(Pids).
+     
+collect([]) -> ok;
+collect([Pid | Pids]) ->
+    receive
+        {ok, Pid} -> collect(Pids)
+    after ?TIMEOUT ->
+        {error, timeout}
+    end.
+
+loop(Mgr, 0) ->
+	Mgr ! {ok, self()};
+loop(Mgr, N) ->
+	ok = fuse:ask(?FUSE, async_dirty),
+	loop(Mgr, N-1).
